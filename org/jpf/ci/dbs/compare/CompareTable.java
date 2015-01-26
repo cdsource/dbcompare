13dd023bee04f42d47d8425bf1163de799c6ef68
liubin <2928981043@qq.com>
2015-01-26 10:48:11 +0800

    更改CompareTable中sSql查询语句增加查询character_maximum_length（最大长度）字段，代替IS_NULLABLE字段作为compareTables() 方法中段长度比较部分

--- org/jpf/ci/dbs/compare/CompareTable.java
+++ org/jpf/ci/dbs/compare/CompareTable.java
@@ -1,8 +1,9 @@
 /** 
  * @author 吴平福 
- * E-mail:wupf@asiainfo-linkage.com 
+ * E-mail:wupf@asiainfo.com 
  * @version 创建时间：2015年1月15日 下午4:00:30 
  * 类说明 
+ * 更改记录：sSql查询语句增加查询character_maximum_length（最大长度）字段，代替IS_NULLABLE字段作为compareTables() 方法中段长度比较部分
  */
 
 package org.jpf.ci.dbs.compare;
@@ -13,9 +14,9 @@ import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jpf.ci.dbs.DbUtils;
 
 public class CompareTable
 {
@@ -25,14 +26,15 @@ public class CompareTable
 			new StringBuffer(), new StringBuffer(), new StringBuffer(),
 			new StringBuffer() };
 
-	private String sSql = " select TABLE_NAME,COLUMN_NAME,IS_NULLABLE,COLUMN_TYPE from information_schema.COLUMNS where table_schema ='ad' order By table_name,column_name";
+	//private String sSql = " select TABLE_NAME,COLUMN_NAME,IS_NULLABLE,COLUMN_TYPE ,character_maximum_length  from information_schema.COLUMNS where table_schema ='ad' order By table_name,column_name";
+	////////////////////////////////MyTest
+	private String sSql = " select TABLE_NAME,COLUMN_NAME,IS_NULLABLE,COLUMN_TYPE , character_maximum_length from information_schema.COLUMNS where table_schema ='testliu' order By table_name,column_name";
 	
 
 
 	public static void main(String[] args) throws Exception
 	{
 		CompareTable cCompareTable = new CompareTable();
-
 	}
     public CompareTable()
     {
@@ -65,6 +67,7 @@ public class CompareTable
 			for (Iterator iter_table = map_develop.keySet().iterator(); iter_table.hasNext();)
 			{
 				String key_table = (String) iter_table.next();
+				System.out.println("key_table:"+key_table);
 				Table table_develop = map_develop.get(key_table);// 获得开发库中的表
 				Table table_product = map_product.get(key_table);// 尝试从生产库中获得同名表
 				if (table_product == null)
@@ -140,9 +143,13 @@ public class CompareTable
 			{// 一张新表
 				tableName = rs.getString("table_name");
 				table = new Table(tableName);
+				/*Column column = new Column(rs.getString("Column_Name"),
+						rs.getString("COLUMN_TYPE"), rs.getString("IS_NULLABLE"));*/
+				//改动此处
 				Column column = new Column(rs.getString("Column_Name"),
-						rs.getString("COLUMN_TYPE"), rs.getString("IS_NULLABLE"));
+						rs.getString("COLUMN_TYPE"), rs.getString("character_maximum_length"));
 				table.columns.put(column.getColumnName(), column);
+				//
 				map.put(rs.getString("table_name"), table);
 			} else
 			{// 已存在的表，增加字段
